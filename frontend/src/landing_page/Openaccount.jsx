c5fe1b98a88dcee600e1c8d7a579e829f521b65a
sauravrawat99 <rawatsaurav145@gmail.com>
2025-08-02 16:25:46 +0530

    i

--- frontend/src/landing_page/Openaccount.jsx
+++ frontend/src/landing_page/Openaccount.jsx
@@ -4,8 +4,6 @@ const Openaccount = () => {
   return (
     <div className="container m-5">
       <div className="row text-center">
-        <img src="media/images/homeHero.png" alt="homehero" className="mb-5" />
-
         <h1 className="mt-5">Open a Zerodha account</h1>
         <p>
           Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and
--- frontend/src/landing_page/Pricing/PrincingPage.jsx
+++ frontend/src/landing_page/Pricing/PrincingPage.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./Home";
 import Brockrage from "./Brockrage";
+import Openaccount from "../Openaccount";
 
 const PrincingPage = () => {
   return (
     <>
       <Home />
+      <Openaccount />
       <Brockrage />
     </>
   );
